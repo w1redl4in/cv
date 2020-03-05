2812c741e0dd1ea2082ac4f39b3537565e97a83f
Felipe <felipe.araujo@hubfintech.com.br>
2020-03-05 10:47:13 -0300

    removed useless endpoints

--- gatsby/create-pages.js
+++ gatsby/create-pages.js
@@ -3,11 +3,6 @@ const createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
 
   createPage({
-    path: '/cv',
-    component: path.resolve('./src/templates/cv-template.js')
-  })
-
-  createPage({
     path: '/',
     component: path.resolve('./src/templates/index-template.js')
   })
