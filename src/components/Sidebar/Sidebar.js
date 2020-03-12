37ddd97d01eda72eb5f13d20e7c040af50efc751
Felipe <felipe.araujo@hubfintech.com.br>
2020-03-12 16:47:45 -0300

    added lottie

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,27 @@
 import React from 'react'
-import { Layout, Divider } from 'antd'
+import { Layout, Divider, Row, Typography } from 'antd'
 import Author from './Author'
 import Contacts from './Contacts'
 import Menu from './Menu'
 import Categories from './Categories'
 import Tags from './Tags'
 import { useSiteMetadata } from '../../hooks'
+import Lottie from 'react-lottie'
+import animationData from './cat.json'
 
 const { Sider } = Layout
 
 const Sidebar = ({ hideMobile }) => {
   const { author, menu } = useSiteMetadata()
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
 
   return (
     <Sider
@@ -25,6 +36,15 @@ const Sidebar = ({ hideMobile }) => {
         <Categories />
         <Tags />
       </div> */}
+      <Row align="middle" justify="center">
+        <Typography.Paragraph
+          type="secondary"
+          style={{ color: 'white', fontStyle: 'italic', marginLeft: '25px' }}
+        >
+          A day without studying is another day between you and your goal
+        </Typography.Paragraph>
+        <Lottie options={defaultOptions} width={150} height={150} />
+      </Row>
     </Sider>
   )
 }
